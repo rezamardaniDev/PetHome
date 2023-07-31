d21f9072ce0607cb84857bc05d6b79153157278b
reza <mardanireza58@gmail.com>
2023-07-31 11:37:21 +0330

    initial commit

---
+++ petshop/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for petshop project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petshop.settings')
+
+application = get_asgi_application()
