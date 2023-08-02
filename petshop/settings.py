dc5caa26eb425609aaa375c9ff49e02b3bfb94ea
reza <mardanireza58@gmail.com>
2023-08-02 08:47:59 +0330

    update auth templae

--- petshop/settings.py
+++ petshop/settings.py
@@ -107,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-IR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
