311d0245ae09d906bd77641d9e449cfde7718324
reza <mardanireza58@gmail.com>
2023-08-03 09:01:05 +0330

    update register view

--- account_module/urls.py
+++ account_module/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "account"
 
 urlpatterns = [
-    path("signup", views.SignUpView.as_view(), name="signup"),
+    path("signup", views.RegisterView.as_view(), name="signup"),
     path("login", views.login, name="login"),
     path("forget-password", views.ForgetPasswordView.as_view(), name="forget-password"),
     path("reset-password", views.ResetPasswordView.as_view(), name="reset-password"),
