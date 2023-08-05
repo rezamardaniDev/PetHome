2dba06453ab680dfd442f150c34baa01312b33a0
reza <mardanireza58@gmail.com>
2023-08-05 21:03:54 +0330

    add verbose_name for product module

--- product_module/apps.py
+++ product_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ProductModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_module'
+    verbose_name = "مدیریت محصولات"
