a8503ed377194f44a0b01afe5191bfd4f34fdeaa
reza <mardanireza58@gmail.com>
2023-08-06 21:27:20 +0330

    add email service

--- utils/email_service.py
+++ utils/email_service.py
@@ -5,10 +5,7 @@ from django.conf import settings
 
 
 def send_email(subject, to, context, template_name):
-    try:
-        html_message = render_to_string(template_name, context)
-        plain_message = strip_tags(html_message)
-        from_email = settings.EMAIL_HOST_USER
-        send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    except:
-        pass
+    html_message = render_to_string(template_name, context)
+    plain_message = strip_tags(html_message)
+    from_email = settings.EMAIL_HOST_USER
+    send_mail(subject, plain_message, from_email, [to], html_message=html_message)
