22401102c49fd0b03d1e765e9f69e829e2e6f29e
reza <mardanireza58@gmail.com>
2023-08-09 09:45:14 +0330

    add contact-us module

---
+++ contactus_module/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ContactusModuleConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'contactus_module'
--- petshop/urls.py
+++ petshop/urls.py
@@ -1,11 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path, include
-import home_module, product_module, account_module
+import home_module, product_module, account_module, contactus_module
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("home_module.urls", namespace="home")),
     path("products/", include("product_module.urls", namespace="products")),
-    path("account/", include("account_module.urls", namespace="account"))
+    path("account/", include("account_module.urls", namespace="account")),
+    path("contact-us/", include("contactus_module.urls", namespace="contact-us"))
 ]
