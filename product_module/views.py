13759a9d9a425b15df71cb9cc6d95dd930ba554c
reza <mardanireza58@gmail.com>
2023-08-09 08:10:24 +0330

    delete find product #test

--- product_module/views.py
+++ product_module/views.py
@@ -1,19 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, get_object_or_404
-from django.urls import reverse_lazy
-from django.views import View
 from django.views.generic import TemplateView
 
-from .models import *
 
-
-class ProductView(LoginRequiredMixin, TemplateView):
-    login_url = reverse_lazy('account:login')
+class ProductView(TemplateView):
     template_name = "products.html"
-
-class FindProduct(View):
-    def get(self, request, product):
-        pro = get_object_or_404(Product, slug=product)
-        return render(request, 'pro.html', context={
-            'pr': pro
-        })
