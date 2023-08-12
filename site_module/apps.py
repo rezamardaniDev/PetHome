4cb3919abaac69b24c462be9338ac38c766e32f2
reza <mardanireza58@gmail.com>
2023-08-12 08:55:53 +0330

    add admin panel button

--- site_module/apps.py
+++ site_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SiteModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'site_module'
+    verbose_name = "تنظیمات سایت"
--- site_module/models.py
+++ site_module/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class SiteSettings(models.Model):
+    site_name = models.CharField(max_length=250, verbose_name="عنوان سایت")
+    site_url = models.CharField(max_length=250, verbose_name="دامنه سایت")
+    about_us = models.TextField(verbose_name="متن درباره ما")
+    address = models.CharField(max_length=250, verbose_name="آدرس ما", null=True)
+    phone = models.CharField(max_length=250, verbose_name="تلفن تماس", null=True)
+    email = models.CharField(max_length=250, verbose_name="ایمیل", null=True)
+    copy_right = models.CharField(max_length=255, verbose_name="متن کپی رایت")
+    site_logo = models.ImageField(upload_to="site-settings", verbose_name="لوگو سایت")
+    is_main_setting = models.BooleanField(verbose_name="تنظیمات اصلی")
+
+    def __str__(self):
+        return self.site_name
+
+    class Meta:
+        verbose_name = "تنظیمات"
+        verbose_name_plural = "تنظیمات"
