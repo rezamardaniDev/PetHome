ec6e07b62a9026a8d40367eff15bf498ad16904a
reza <mardanireza58@gmail.com>
2023-08-13 08:01:43 +0330

    add blog model

--- blog_module/apps.py
+++ blog_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class BlogModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog_module'
+    verbose_name = "مدیریت مقالات"
