5618a579dfe6916f06cfa42478bab3ac8c11c60f
reza <mardanireza58@gmail.com>
2023-08-14 11:43:51 +0330

    debug and add category

--- blog_module/views.py
+++ blog_module/views.py
@@ -12,19 +12,26 @@ class BlogListView(ListView):
     paginate_by = 6
     context_object_name = "blog"
 
-    def get_context_data(self, *kwargs):
-        context = super().get_context_data(*kwargs)
-        context['categories'] = BlogCategory.objects.filter(is_active=True).all()
-        return context
+    def get_queryset(self):
+        query = super(BlogListView, self).get_queryset()
+        category_name = self.kwargs.get('category')
+        if category_name is not None:
+            query = query.filter(category__url_title__iexact=category_name).all()
+        return query
 
 class BlogDetailView(View):
     def get(self,request, post_id):
         post: Blog = Blog.objects.filter(is_active=True, slug=post_id).first()
         categories = BlogCategory.objects.all()[0:3]
-        post.view += 1
-        post.save()
 
         return render(request, 'blog_detail.html', context={
         'post': post,
         'categories': categories
+    })
+
+def blog_categories_component(request):
+    blog_categories = BlogCategory.objects.filter(is_active=True)[0:3]
+
+    return render(request, 'components/blog_categories_component.html', context={
+    'categories': blog_categories
     })
