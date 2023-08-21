e0c54f4b44ae62cae9e9ac4dbbf5a1538ffabb9f
reza <mardanireza58@gmail.com>
2023-08-21 20:32:34 +0330

    add post method for product comment view, edite prduct template comment form

--- product_module/views.py
+++ product_module/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, View
 
-from .models import Product, ProductCategory
+from .forms import CommentForm
+from .models import Product, ProductCategory, ProductComment
 
 
 class ProductView(ListView):
@@ -17,20 +18,37 @@ class ProductView(ListView):
             query = query.filter(category__url_title__iexact=category_name)
         return query
 
+
 class ProductDetailView(View):
     def get(self, request, product_id):
         product = Product.objects.filter(is_active=True, id=product_id).first()
+        comments = product.comments.all()
+        return render(request, 'product_detail.html', context={
+            'product': product,
+            'comments': comments
+        })
 
-        return render(request, 'product_detail.html', context=
-        {'product': product}
-                      )
+    def post(self, request, product_id):
+        comment_form: CommentForm = CommentForm(request.POST)
+        product: Product = Product.objects.filter(is_active=True, id=product_id).first()
+        user = request.user
+        if comment_form.is_valid():
+            new_comment = ProductComment()
+            new_comment.message = comment_form.cleaned_data.get('message')
+            new_comment.user = user
+            new_comment.product = product
+            new_comment.save()
+            return redirect('products:products_detail', product.id)
+        else:
+            comment_form.add_error('message', 'مشکلی در ثبت کامنت شما پیش آمده است')
+        return render(request, 'product_detail.html', context={
+            'product': product
+        })
 
-    def post(self, request):
-        ...
 
 def product_categories_component(request):
     product_categories: ProductCategory = ProductCategory.objects.filter(is_active=True)[0:3]
 
     return render(request, 'component/product_categories_component.html', context={
-    'categories': product_categories
+        'categories': product_categories
     })
