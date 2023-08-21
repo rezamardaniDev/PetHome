82a1d06b90958661bb014863d752b7aa336886d5
reza <mardanireza58@gmail.com>
2023-08-21 20:46:35 +0330

    add askquestio model to admin

--- home_module/apps.py
+++ home_module/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class HomeModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home_module'
+    verbose_name = "صفحه اصلی"
