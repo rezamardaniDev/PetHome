000457a018083636251bd7568c204a7ceca3bd07
reza <mardanireza58@gmail.com>
2023-08-22 17:45:27 +0330

    admin url set on admin profile

--- gallery_module/models.py
+++ gallery_module/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django_resized import ResizedImageField
 
 # Create your models here.
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=250, verbose_name="نام عکس")
+    image = ResizedImageField(upload_to="gallery", verbose_name="عکس", quality=100, size=[350, 300], crop=['middle', 'center'])
+    is_active = models.BooleanField(verbose_name="فعال / غیرفعال")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "تصویر"
+        verbose_name = "گالری تصاویر"
--- gallery_module/urls.py
+++ gallery_module/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from . import views
+
 
 app_name = 'gallery'
 
-ulrpatterns = [
-
+urlpatterns = [
+    path('', views.test, name='gallery')
 ]
