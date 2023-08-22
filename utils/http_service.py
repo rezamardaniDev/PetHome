6c1037e5ee840968c3d239caf42a600ab6f10361
reza <mardanireza58@gmail.com>
2023-08-22 08:48:53 +0330

    create http_service file

---
+++ utils/http_service.py
@@ -0,0 +1,13 @@
+from django.http import HttpRequest
+
+
+def get_client_ip(request: HttpRequest):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+
+    return ip
+
