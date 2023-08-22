583abd780acbc464d4dd48b61ddf61ef1d78e49e
reza <mardanireza58@gmail.com>
2023-08-22 12:02:49 +0330

    add verify admin to dashboard tempalte

--- account_module/admin.py
+++ account_module/admin.py
@@ -5,5 +5,5 @@ from .models import User
 # Register your models here.
 @admin.register(User)
 class UserManager(admin.ModelAdmin):
-    list_display = ['username', 'first_name', 'email', 'is_active', 'is_staff']
-    list_editable = ['is_active', 'is_staff']
+    list_display = ['username', 'first_name', 'email', 'is_active', 'is_staff', 'is_superuser']
+    list_editable = ['is_active', 'is_staff', 'is_superuser']
