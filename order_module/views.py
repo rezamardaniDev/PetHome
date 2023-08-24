08210234160ed726708bed17cf49c17b02c15984
reza <mardanireza58@gmail.com>
2023-08-24 08:38:40 +0330

    write view for order

--- order_module/views.py
+++ order_module/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
+from product_module.models import Product
+
 
 def add_product_to_order(request):
     print(request.GET)
     product_id = request.GET.get('product_id')
     product_count = request.GET.get('count')
-    print(f'productId={product_id}, count={product_count}')
-    return HttpResponse('done')
+    if request.user.is_authenticated:
+        product = Product.objects.filter(id=product_id, is_active=True).first()
+        if product is not None:
+            # get user open order
+            # add product to order
+            pass
+    else:
+        return JsonResponse({
+            'status': 'user is not login',
+        })
