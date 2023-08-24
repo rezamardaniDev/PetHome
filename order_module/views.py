48fa91282d511e249faa415ec21e4b1ae660d16c
reza <mardanireza58@gmail.com>
2023-08-24 15:03:22 +0330

    add sweet Alert to project

--- order_module/views.py
+++ order_module/views.py
@@ -5,16 +5,21 @@ from product_module.models import Product
 
 
 def add_product_to_order(request):
-    print(request.GET)
-    product_id = request.GET.get('product_id')
-    count = request.GET.get('count')
+    product_id = int(request.GET.get('product_id'))
+    count = int(request.GET.get('count'))
+
+    if count < 1:
+        return JsonResponse({
+            'status': 'invalid count'
+        })
+
     if request.user.is_authenticated:
         product = Product.objects.filter(id=product_id, is_active=True).first()
         if product is not None:
             current_order, created = Order.objects.get_or_create(user_id=request.user.id, is_paid=False)
             current_order_detail = current_order.orderdetail_set.filter(product_id=product_id).first()
             if current_order_detail is not None:
-                current_order_detail.count += int(count)
+                current_order_detail.count += count
                 current_order_detail.save()
             else:
                 new_detail = OrderDetail(order_id=current_order.id,  product_id=product_id, count=count)
--- static/Js/order.js
+++ static/Js/order.js
@@ -1,5 +1,13 @@
-function addProductToOrder(productId){
-    $.get('/order/add-to-order?product_id=' + productId + '&count=' + 1).then(res =>{
+function addProductToOrder(productId) {
+    $.get('/order/add-to-order?product_id=' + productId + '&count=' + 1).then(res => {
         console.log(res)
     });
 }
+
+Swal.fire({
+    position: 'top-center',
+    icon: 'success',
+    title: 'محصول به سبد خرید افزوده شد',
+    showConfirmButton: false,
+    timer: 1200
+})
