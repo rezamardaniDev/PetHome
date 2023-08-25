b5915f615f73f4eaf84bba79d4309f608628e5ca
reza <mardanireza58@gmail.com>
2023-08-25 14:42:56 +0330

    add zarinpal view

--- order_module/urls.py
+++ order_module/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-    path('add-to-order', views.add_product_to_order, name='add_to_order')
+    path('add-to-order', views.add_product_to_order, name='add_to_order'),
+    path('request', views.request_payment, name='request_payment'),
+    path('verify', views.verify_payment, name='verify_payment'),
 ]
