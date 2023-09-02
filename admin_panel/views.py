d29bdfc0abc6fb3947a8804d5661f66a92d832ea
reza <mardanireza58@gmail.com>
2023-09-02 20:46:11 +0330

    upddate

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -3,12 +3,9 @@ from django.views import View
 
 from order_module.models import OrderDetail, Order, OrderCheckout
 
-
-# Create your views here.
 class AdminOrderView(View):
     def get(self, request):
         order = OrderCheckout.objects.filter(order__is_paid=True)
-        print(order)
         return render(request, 'order-admin.html', context={
             'order': order,
         })
