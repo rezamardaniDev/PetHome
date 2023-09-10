647c40d2d99baa21d56b64e81316c7faaf3cb0e2
reza <mardanireza58@gmail.com>
2023-09-10 20:12:55 +0330

    serialized Products

--- product_module/urls.py
+++ product_module/urls.py
@@ -5,6 +5,7 @@ app_name = "products"
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name="products_list"),
+path('api', views.product_all),
     path('<slug:product_id>',views.ProductDetailView.as_view(), name="products_detail"),
     path('cut/<str:category>', views.ProductView.as_view(), name="products_list_category"),
 ]
