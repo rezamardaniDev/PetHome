25f23903b049cdf6f86396486f3ee9d7398038e2
reza <mardanireza58@gmail.com>
2023-09-14 18:46:33 +0330

    import module to view API

--- API/views.py
+++ API/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from account_module.models import User
+from product_module.models import Product
+from blog_module.models import Blog
+from order_module.models import Order
+
+from rest_framework import generics
 
 # Create your views here.
