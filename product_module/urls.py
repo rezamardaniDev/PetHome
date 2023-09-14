63317fa95226629058709978410bdce5cdda0fef
reza <mardanireza58@gmail.com>
2023-09-14 12:55:01 +0330

    clean update three

--- product_module/urls.py
+++ product_module/urls.py
@@ -9,17 +9,6 @@ app_name = "products"
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name="products_list"),
-    path('api', views.product_all),
-    path('api/<product_id>', views.product_detail_api),
-    path('cbv', views.ProductApiListView.as_view()),
-    path('cbv/<product_id>', views.ProductApiDetailView.as_view()),
-    path('generics', views.ProductListGenericView.as_view()),
-    path('generics/<pk>', views.ProductDetailGenericView.as_view()),
-    path('mixin', views.ProductListMixinApiView.as_view()),
-    path('mixin/<pk>', views.ProductDetailMixinApiView.as_view()),
-    path('viewset/', include(router.urls)),
-    path('user', views.UserListViewSetApiView.as_view()),
-    path('comments', views.CommentListViewSetApiView.as_view()),
     path('<slug:product_id>', views.ProductDetailView.as_view(), name="products_detail"),
     path('cut/<str:category>', views.ProductView.as_view(), name="products_list_category"),
 
