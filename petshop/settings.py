3698422fc3eb2d697210f9ca005ec7337735b0cd
reza <mardanireza58@gmail.com>
2023-09-14 20:03:35 +0330

    add permission and authentication

--- petshop/settings.py
+++ petshop/settings.py
@@ -155,5 +155,6 @@ SANDBOX = True
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
-
+    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticated']
 }
--- petshop/urls.py
+++ petshop/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +15,9 @@ urlpatterns = [
     path("gallery/", include("gallery_module.urls", namespace="gallery")),
     path('order/', include("order_module.urls", namespace="order")),
     path('panel/', include("admin_panel.urls", namespace="panel")),
-    path('', include("API.urls", namespace="api"))
+    path('', include("API.urls", namespace="api")),
+    path('auth-token/', obtain_auth_token, name='generate_auth_token')
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
