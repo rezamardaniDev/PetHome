74f50ef5ff895cd87a839d7dff002146b6e38678
reza <mardanireza58@gmail.com>
2023-11-15 10:09:59 +0330

    add id for send and cancel

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -6,6 +6,7 @@ app_name = 'panel'
 
 urlpatterns = [
     path('orders', views.AdminOrderView.as_view(), name='order-admin'),
-    path('detail/<pk>',views.AdminOrderDetailView.as_view(), name='order-admin-detail' )
-
+    path('detail/<pk>', views.AdminOrderDetailView.as_view(), name='order-admin-detail'),
+    path('sended/<pk>', views.sended, name='sended'),
+    path('cancel/<pk>', views.cancel, name='cancel')
 ]
